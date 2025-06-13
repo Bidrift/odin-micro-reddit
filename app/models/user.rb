844f0af1e09549b0542240d86b6b099f2dbb79d7
Bidrift <yassine.fighter@gmail.com>
2025-06-13 18:15:14 +0100

    Fix linting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     has_many :posts
     has_many :comments
-    
+
     validates :username, :password, :email, presence: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email' }, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email" }, uniqueness: true
     validates :username, length: { in: 4..10 }, uniqueness: true
-    validates :password, length: { in: 6..16}
+    validates :password, length: { in: 6..16 }
 end
